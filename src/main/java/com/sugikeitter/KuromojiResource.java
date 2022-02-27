634ad193554e20c73e372793aa40882f3239f2af
sugikeitter <s.l.n.f.13_kei.s@me.com>
2022-02-28 00:21:34 +0900

    Add css for table layout

--- src/main/java/com/sugikeitter/KuromojiResource.java
+++ src/main/java/com/sugikeitter/KuromojiResource.java
@@ -12,40 +12,56 @@ import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import java.util.List;
 
 
+
 @Path("/tokenizeKuromoji")
 public class KuromojiResource {
-
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/{text}")
     public String tokenizeFromPath(@PathParam String text) {
-        Tokenizer tokenizer = new Tokenizer() ;
-//        StringBuilder sb = new StringBuilder("<table><thead><tr><th>Surface form</th><th>Part-of-Speech</th><th>Base form</th><th>Reading Pronunciation</th></tr></thead>");
-
+        System.out.println("input: " + text);
         if (text == null || text.isEmpty()) {
             System.out.println("No PathParam");
             text = "お寿司が食べたい。";
         }
+        Tokenizer tokenizer = new Tokenizer() ;
         List<Token> tokens = tokenizer.tokenize(text);
-        StringBuilder sb = new StringBuilder("<table>");
-        sb.append("<thead><tr><th>Surface</th><th>AllFeatures</th></tr></thead>");
-        sb.append("<tbody>");
+        StringBuilder htmlResult = new StringBuilder();
+        htmlResult.append("<html>")
+                .append("<head>")
+                .append("<style type=\"text/css\">")
+                .append(css)
+                .append("</style>")
+                .append("</head>")
+                .append("<body>")
+                .append("<table>")
+                .append("<thead><tr><th>Surface</th><th>Part-of-Speech</th><th>Base form</th><th>Reading Pronunciation</th></tr></thead>")
+                .append("<tbody>");
         for (Token token : tokens) {
-//            System.out.println(token.getSurface() + "\t" + token.getAllFeatures());
-            sb.append("<tr>");
-            sb.append("<td>");
-            sb.append(token.getSurface());
-            sb.append("</td>");
-            sb.append("<td>");
-            sb.append(token.getAllFeatures());
-            sb.append("</td>");
-            sb.append("</tr>");
+            htmlResult.append("<tr>")
+                    .append("<td>")
+                    .append(token.getSurface())
+                    .append("</td>")
+                    .append("<td>")
+                    .append(token.getAllFeaturesArray()[0])
+                    .append(token.getAllFeaturesArray()[1])
+                    .append(token.getAllFeaturesArray()[2])
+                    .append(token.getAllFeaturesArray()[3])
+                    .append("</td>")
+                    .append("<td>")
+                    .append(token.getAllFeaturesArray()[6])
+                    .append("</td>")
+                    .append("<td>")
+                    .append(token.getAllFeaturesArray()[7])
+                    .append("</td>")
+                    .append("</tr>");
         }
-        sb.append("</tbody>");
-        sb.append("</table>");
-        System.out.println(sb);
-        return sb.toString();
-//        return "Tokenized";
+        htmlResult.append("</tbody>");
+        htmlResult.append("</table>");
+        htmlResult.append("</body>");
+        htmlResult.append("</html>");
+//        System.out.println(htmlResult);
+        return htmlResult.toString();
     }
 
     @GET
@@ -59,4 +75,100 @@ public class KuromojiResource {
         }
         return "<p>Success tokenize!</p>";
     }
+
+    private static final String css = "body {\n" +
+            "  font-family: \"Open Sans\", sans-serif;\n" +
+            "  line-height: 1.25;\n" +
+            "}\n" +
+            "\n" +
+            "table {\n" +
+            "  border-collapse: collapse;\n" +
+            "  margin: 0 auto;\n" +
+            "  padding: 0;\n" +
+            "  width: 650px;\n" +
+            "  table-layout: fixed;\n" +
+            "  color: #000;\n" +
+            "}\n" +
+            "\n" +
+            "table tr {\n" +
+            "  background-color: #f7f7f7;\n" +
+            "  padding: .35em;\n" +
+            "  border-bottom: 1px solid #bbb;\n" +
+            "}\n" +
+            "table thead{\n" +
+            "  border-bottom: 2px solid #67a83b;\n" +
+            "  color: #67a83b;\n" +
+            "}\n" +
+            "table tr:last-child{\n" +
+            "   border-bottom: none\n" +
+            "}\n" +
+            "table th,\n" +
+            "table td {\n" +
+            "  padding: 1em 10px 1em 1em;\n" +
+            "  border-right: 1px solid #bbb;\n" +
+            "}\n" +
+            "table th:last-child,\n" +
+            "table td:last-child{\n" +
+            "    border: none;\n" +
+            "}\n" +
+            "tbody th {\n" +
+            "    color: #ff9901;\n" +
+            "}\n" +
+            ".txt{\n" +
+            "   text-align: left;\n" +
+            "   font-size: .85em;\n" +
+            "}\n" +
+            ".price{\n" +
+            "   text-align: right;\n" +
+            "}\n" +
+            "@media screen and (max-width: 600px) {\n" +
+            "  table {\n" +
+            "    border: 0;\n" +
+            "    width:100%\n" +
+            "  }\n" +
+            "  table th{\n" +
+            "    display: block;\n" +
+            "    border-right: none;\n" +
+            "    border-bottom: 5px solid #ff9901;\n" +
+            "    padding-bottom: .6em;\n" +
+            "    margin-bottom: .6em;\n" +
+            "\n" +
+            "  }\n" +
+            "  table thead {\n" +
+            "    border: none;\n" +
+            "    clip: rect(0 0 0 0);\n" +
+            "    height: 1px;\n" +
+            "    margin: -1px;\n" +
+            "    overflow: hidden;\n" +
+            "    padding: 0;\n" +
+            "    position: absolute;\n" +
+            "    width: 1px;\n" +
+            "  }\n" +
+            "  \n" +
+            "  table tr {\n" +
+            "    display: block;\n" +
+            "    margin-bottom: 2em;\n" +
+            "  }\n" +
+            "  \n" +
+            "  table td {\n" +
+            "    border-bottom: 1px solid #bbb;\n" +
+            "    display: block;\n" +
+            "    font-size: .8em;\n" +
+            "    text-align: right;\n" +
+            "    position: relative;\n" +
+            "    padding: .625em .625em .625em 4em;\n" +
+            "    border-right: none;\n" +
+            "  }\n" +
+            "  \n" +
+            "  table td::before {\n" +
+            "    content: attr(data-label);\n" +
+            "    font-weight: bold;\n" +
+            "    position: absolute;\n" +
+            "    left: 10px;\n" +
+            "  }\n" +
+            "  \n" +
+            "  table td:last-child {\n" +
+            "    border-bottom: 0;\n" +
+            "  }\n" +
+            "}";
 }
